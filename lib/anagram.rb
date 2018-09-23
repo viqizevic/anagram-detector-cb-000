e11c8018a7cdd898a446187056dd34168cd889fd
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-23 22:18:26 +0200

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    order = word.split('').sort
+    array.select { |cand| cand.split('').sort == order }
+  end
+
+end
